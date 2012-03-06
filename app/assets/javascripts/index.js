ab640f95e6e6c2b44afac41b8c0eeede92420159
smalloranges <smalloranges.lee@gmail.com>
2012-03-07 01:44:28 +0800

    js for home

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,7 @@
 $(function () {
+  //home
+  $('.banner li:eq(4)').after('<li class="intro"><p>笔记自然，发现植物之趣</p></li>');
+
   //search
   $('#search-link').click(function(){
     $('#search').toggle();
