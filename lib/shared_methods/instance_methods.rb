82798d04617d1fb0770550071ae66552bab5bb4d
lihongwang <lihongwang0618@gmail.com>
2012-05-31 23:36:32 +0800

    delete instance_methods

--- lib/shared_methods/instance_methods.rb
+++
@@ -1,8 +0,0 @@
-module InstanceMethods
-	NAME_KINDS = ['latin', 'english', 'zh']
-	NAME_KINDS.each do |n_k|
-    define_method "#{n_k}_name" do
-      self.name.nil? ? nil : self.name[n_k]
-    end
-  end
-end
