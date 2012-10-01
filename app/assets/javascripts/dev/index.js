1ae35ce86ab09246824f08c837567fd5db824fdb
smalloranges <smalloranges.lee@gmail.com>
2012-10-01 19:37:42 +0800

    hidden sth

--- app/assets/javascripts/dev/index.js
+++ app/assets/javascripts/dev/index.js
@@ -23,23 +23,23 @@ $(function () {
   $('#flash-notice').delay(3000).fadeOut('slow');
 
   // slide
-  $('.p-slide .right-nav').live('click', function(){
-    var by = $(this);
-    // 先加载loading.gif图片
-    // by.parent().siblings('ul').find('li').fadeTo(400, 0).html('<img src="/images/loading.gif" />');
-    // 然后ajax取到下一批图片
-    // 目前等待后台数据传递中
-  });
+  // $('.p-slide .right-nav').live('click', function(){
+  //   var by = $(this);
+  //   // 先加载loading.gif图片
+  //   // by.parent().siblings('ul').find('li').fadeTo(400, 0).html('<img src="/images/loading.gif" />');
+  //   // 然后ajax取到下一批图片
+  //   // 目前等待后台数据传递中
+  // });
 
   // follow
-  $('a.follow').live('click', function(){
-    var by = $(this);
-    if (by.hasClass('ico-ui-follow')){
-      by.removeClass('ico-ui-follow').addClass('ico-ui-unfollow');
-    }else{
-      by.removeClass('ico-ui-unfollow').addClass('ico-ui-follow');
-    }
-  });
+  // $('a.follow').live('click', function(){
+  //   var by = $(this);
+  //   if (by.hasClass('ico-ui-follow')){
+  //     by.removeClass('ico-ui-follow').addClass('ico-ui-unfollow');
+  //   }else{
+  //     by.removeClass('ico-ui-unfollow').addClass('ico-ui-follow');
+  //   }
+  // });
 
   // edit photo's description
   $('.edit-description').click(function(){
@@ -48,7 +48,7 @@ $(function () {
         text = description.text();
     if(by.text() == "[编辑]"){
       $(this).text('[取消]');
-      description.after('<div id="description"><textarea class="comm-content">'+ text +'</textarea>' + 
+      description.after('<div id="description"><textarea class="comm-content">'+ text +'</textarea>' +
                        '<button type="submit" id="descrption-btn">保存</button></div>');
       description.hide();
     }else{
@@ -56,7 +56,7 @@ $(function () {
       description.show();
       $(this).text('[编辑]');
     }
-    
+
     $('#descrption-btn').click(function(){
       var by = $(this);
       description.html(by.prev().val()).show();
@@ -67,19 +67,18 @@ $(function () {
   });
 
   // comment
-  $('#comment-btn').click(function(){
+  $('#comment-btn').on('click', function(){
     var by = $(this),
         commBox = by.prev(),
         text = commBox.val(),
         pid = $('#comment-li').attr('rel'),
         url = '/pictures/'+ pid +'/comments';
-    
+
     if ( text.length == 0 ){
       by.before('<b>你还没有输入内容</b>').slideUp(50);
       return false;
     }
 
-    
     $.ajax({
       type: 'post',
       cache: false,
@@ -95,7 +94,7 @@ $(function () {
         by.before('<b>错误</b>').slideUp();
       },
       success: function(data){
-        $(data).prependTo( by.closest('ul') ).hide().slideDown();
+        by.closest('li').before($(data)).hide().slideDown();
         commBox.val('');
       },
       complete: function(){
