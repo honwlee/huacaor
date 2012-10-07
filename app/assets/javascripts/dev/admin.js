5ca4d6130873702091fc8aaab5cecd89f98ed1ca
smalloranges <smalloranges.lee@gmail.com>
2012-10-07 23:45:53 +0800

    validate

--- app/assets/javascripts/dev/admin.js
+++ app/assets/javascripts/dev/admin.js
@@ -4,23 +4,7 @@ $(function(){
   $('#flash-notice').delay(3000).fadeOut('slow');
 
   // 表单认证
-  $('.edit_tag, #new_tag').validate({
-    rules: {
-      'tag[name]': {
-        required: true
-      },
-      'tag[css_name]': {
-        required: true
-      }
-    },
-    messages: {
-      'tag[name]': {
-        required: '请输入名称'
-      },
-      'tag[css_name]': {
-        required: '请输入样式标识'
-      }
-    },
+  $('form').validate({
     errorPlacement: function(label, el){
       el.siblings('p.tips').append(label);
     },
@@ -29,33 +13,6 @@ $(function(){
     }
   });
 
-  // 表单认证
-  $('.edit_user').validate({
-    rules: {
-      'user[user_email]': {
-        required: true,
-        email:true
-      },
-      'user[user_name]': {
-        required: true
-      }
-    },
-    messages: {
-      'user[user_email]': {
-        required: '请输入邮箱',
-        email: '邮箱格式不正确'
-      },
-      'user[user_name]': {
-        required: '请输入名字'
-      }
-    },
-    errorPlacement: function(label, el){
-      el.siblings('p.tips').append(label);
-    },
-    success: function(label){
-      label.remove();
-    }
-  });
 
 });
 
--- app/assets/javascripts/dev/setting.js
+++ app/assets/javascripts/dev/setting.js
@@ -1,25 +1,6 @@
 $(function(){
 
-  // 表单认证
-  $('.edit_user').validate({
-    rules: {
-      'user_email': {
-        required: true,
-        email:true
-      },
-      'user_name': {
-        required: true
-      }
-    },
-    messages: {
-      'user_email': {
-        required: '请输入邮箱',
-        email: '邮箱格式不正确'
-      },
-      'user_name': {
-        required: '请输入名字'
-      }
-    },
+  $('form').validate({
     errorPlacement: function(label, el){
       el.siblings('p.tips').append(label);
     },
@@ -28,45 +9,6 @@ $(function(){
     }
   });
 
-  // 重置密码
-  $('#getpwd').validate({
-    rules: {
-      '[old_password]': {
-        required: true,
-        minlength: 5
-      },
-      '[new_password]': {
-        required: true,
-        minlength: 5
-      },
-      '[new_password_confirm]': {
-        required: true,
-        minlength: 5,
-        equalTo: '#new_password'
-      }
-    },
-    messages: {
-      '[old_password]': {
-        required: '请输入原密码',
-        minlength: '密码必须大于5个字符'
-      },
-      '[new_password]': {
-        required: '请输入新密码',
-        minlength: '密码必须大于5个字符'
-      },
-      '[new_password_confirm]': {
-        required: '请输入新密码',
-        minlength: '两次输入不同'
-      }
-    },
-    errorPlacement: function(label, el){
-      el.siblings('p.tips').append(label).children(':not(label)').hide();
-    },
-    success: function(label){
-      label.siblings().show();
-      label.remove();
-    }
-  });
 
   // 添加表单项目
   $('a.add-item').live('click', function(){
